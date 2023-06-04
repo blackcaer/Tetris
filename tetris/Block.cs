1d19aff1ede26a4f051f2e8d999d5a58c0ffddd5
Kryspin Kucha <blackcaer@onet.pl>
2023-06-04 13:04:33 +0200

    Add project files.

---
+++ tetris/Block.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+namespace tetris
+{
+    public abstract class Block
+    {
+        protected abstract Position[][] Tiles { get; }
+        protected abstract Position StartOffset { get; }
+        public abstract int Id { get; }
+
+        private int rotationState;
+        private Position offset;
+        
+        public Block()
+        {
+            offset = new Position(StartOffset.Row, StartOffset.Column);
+        }
+
+
+    }
+}
---
+++ tetris/GameGrid.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace tetris
+{
+    public class GameGrid
+    {
+        private readonly int[,] grid;
+        public int Rows { get; }
+        public int Columns { get; }
+
+        public int this[int r,int c]
+        {
+            get => grid[r, c];
+            set => grid[r, c] = value;
+        }
+        public GameGrid(int rows,int cols)
+        {
+            Rows = rows;
+            Columns = cols;
+            grid = new int[rows, cols];
+
+        }
+        public bool IsInside(int r,int c)
+        {
+            return r>= 0 && c >= 0 && c < Columns && r < Rows;
+        }
+        public bool IsEmpty(int r,int c)
+        {
+            return IsInside(r, c) && grid[r, c] == 0;
+        }
+        public bool IsRowFull(int r)
+        {
+            for (int i=0;i<Rows;i++)
+            {
+                if (IsEmpty(r,i))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public bool IsRowEmpty(int r)
+        {
+            for (int i = 0; i < Columns; i++)
+            {
+                if (!IsEmpty(r, i))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void ClearRow(int r)
+        {
+            for (int i = 0; i < Columns; i++)
+            {
+                grid[r, i] = 0;
+            }
+            
+        }
+        public void MoveRowDown(int r,int howMany)
+        {
+            for (int c = 0;c < Columns;c++)
+            {
+                grid[r- howMany, c- howMany] = grid[r, c];
+                grid[r, c] = 0;
+            } 
+        }
+
+        public int ClearFullRows()
+        {
+            int cleared = 0;
+            for (int r=Rows-1;r>=0;r--)
+            {
+                if (IsRowEmpty(r))
+                {
+                    break;
+                }
+                if (IsRowFull(r))
+                {
+                    ClearRow(r);
+                    cleared++;
+                    continue;
+                }else if (cleared>0)
+                {
+                    MoveRowDown(r, cleared);
+                }
+            }
+            return cleared;
+        }
+    }
+}
